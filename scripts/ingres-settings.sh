76dc2bd1eb90d476e3c18cb88ca3bf6ac8ef79be
Dina Berry <dinaberry@outlook.com>
2024-08-16 08:30:09 -0700

    port

---
+++ scripts/ingres-settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Prerequisites
+## az login --use-device-code
+##
+
+# Set variables - only use alphanumeric characters (no dashes or underscores)
+AZURE_SUBSCRIPTION_ID="19016922-4bf5-4c41-9553-8eff5da1500e"
+AZURE_RESOURCE_GROUP="nextjs-github-app"
+AZURE_CONTAINER_APP_NAME="dfberrynextjs"
+
+az containerapp ingress show \
+--subscription $AZURE_SUBSCRIPTION_ID \
+--name $AZURE_CONTAINER_APP_NAME \
+--resource-group $AZURE_RESOURCE_GROUP 
