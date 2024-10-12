839d685dcb48864bf46fa6aa5c2ea6f75fd3faaa
Dina Berry <dinaberry@outlook.com>
2024-10-12 21:46:50 +0000

    finished

--- src/components/github/Stats.tsx
+++ src/components/github/Stats.tsx
@@ -1,14 +1,15 @@
 import React, { Suspense } from 'react';
 import { GitHubRepoStats, GitHubRepoCommunityMetrics } from '@/lib/github/stats'; // Adjust the import path as necessary
 
-import { FaCalendar, FaArchive, FaBoxOpen, FaGlobe, FaLock, FaQuestionCircle, FaCheckCircle, FaTimesCircle, FaStar, FaCodeBranch, FaEye, FaChartBar } from 'react-icons/fa';
+import { FaBug, FaHeartbeat, FaCalendar, FaArchive, FaBoxOpen, FaGlobe, FaLock, FaQuestionCircle, FaCheckCircle, FaTimesCircle, FaStar, FaCodeBranch, FaEye, FaChartBar } from 'react-icons/fa';
+import styles from './StatsCard.module.css';
+import { capitalizeFirstLetter } from '@/lib/strings';
 
 interface StatsCardProps {
     stats: GitHubRepoStats;
     metrics: GitHubRepoCommunityMetrics;
     ownerAndRepo: string; // repo name
 }
-
 const StatsCard: React.FC<StatsCardProps> = ({ ownerAndRepo, stats, metrics }) => {
 
     console.log(`componentOwner: ${ownerAndRepo}`);
@@ -23,15 +24,23 @@ const StatsCard: React.FC<StatsCardProps> = ({ ownerAndRepo, stats, metrics }) =
     };
 
     return (
-        <article className="border rounded-lg p-4 shadow-md bg-white">
-            <div className="mb-4 bg-gray-100 p-4 rounded shadow">
+        <article className={`border rounded-lg p-4 shadow-md bg-white relative ${stats.archived ? styles.archived : ''}`}>
+            {stats.archived && (
+                <div className="absolute inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50 z-10">
+                    <p className="text-4xl font-bold text-gray-500">Archived</p>
+                </div>
+            )}
+            <div className="mb-4 bg-gray-100 p-4 rounded shadow relative z-20">
                 <header className="mb-4 flex justify-between items-center">
-                    <div>
+                    <div className="flex items-center justify-between w-full">
                         <h2 className="text-xl font-bold text-blue-600">
                             <a href={`https://github.com/${ownerAndRepo}`} target="_blank" rel="noopener noreferrer">
                                 {ownerAndRepo}
                             </a>
                         </h2>
+                        {stats.archived && (
+                            <span className="text-red-400 text-xl font-bold ml-auto">Archived</span>
+                        )}
                     </div>
                 </header>
                 <Suspense fallback={<p>Loading...</p>}>
@@ -54,13 +63,6 @@ const StatsCard: React.FC<StatsCardProps> = ({ ownerAndRepo, stats, metrics }) =
                             </div>
                         </div>
                         <div className="flex flex-col w-1/2">
-                            {/*<div className="flex justify-between">
-                                <p className="text-lg font-semibold">Visibility:</p>
-                                <p className="text-lg flex items-center">
-                                    {stats.visibility === 'public' ? <FaGlobe className="mr-2" /> : <FaLock className="mr-2" />}
-                                    {stats.visibility}
-                                </p>
-                            </div>*/}
                             <div className="flex justify-between">
                                 <p className="text-lg font-semibold">Open Issues:</p>
                                 <p className="text-lg">{stats.open_issues}</p>
@@ -69,30 +71,25 @@ const StatsCard: React.FC<StatsCardProps> = ({ ownerAndRepo, stats, metrics }) =
                                 <p className="text-lg font-semibold">License:</p>
                                 <p className="text-lg">{stats.license?.name}</p>
                             </div>
-                            <div className="flex justify-between">
+                            {/*<div className="flex justify-between">
                                 <p className="text-lg font-semibold">Allow Auto Merge:</p>
                                 <p className="text-lg flex items-center">
                                     {stats.allow_auto_merge === null ? <FaQuestionCircle className="mr-2" /> : stats.allow_auto_merge ? <FaCheckCircle className="mr-2" /> : <FaTimesCircle className="mr-2" />}
                                     {stats.allow_auto_merge !== null ? stats.allow_auto_merge!.toString() : ''}
                                 </p>
-                            </div>
+                            </div>*/}
                         </div>
                     </div>
                 </Suspense>
             </div>
             <Suspense fallback={<p>Loading...</p>}>
-                <section className="grid gap-4">
-                    <div className="grid grid-cols-4 gap-2">
-                        <p className="text-lg font-semibold">Health</p>
-                        <p className="text-lg font-semibold flex items-center"><FaStar className="mr-2" />Stars</p>
-                        <p className="text-lg font-semibold flex items-center"><FaCodeBranch className="mr-2" />Forks</p>
-                        <p className="text-lg font-semibold flex items-center"><FaEye className="mr-2" />Watchers</p>
-                    </div>
-                    <div className="grid grid-cols-4 gap-2">
-                        <p className="text-lg">{metrics?.health_percentage}%</p>
-                        <p className="text-lg">{stats.stars}</p>
-                        <p className="text-lg">{stats.forks}</p>
-                        <p className="text-lg">{stats.watchers}</p>
+                <section className="grid gap-5 relative z-20">
+                    <div className="grid grid-cols-5 gap-2">
+                        <p className="text-lg"><FaHeartbeat className="mr-2" />{metrics?.health_percentage}%</p>
+                        <p className="text-lg"><FaStar className="mr-2" />{stats.stars}</p>
+                        <p className="text-lg"><FaCodeBranch className="mr-2" />{stats.forks}</p>
+                        <p className="text-lg"><FaEye className="mr-2" />{stats.watchers}</p>
+                        <p className="text-lg"><FaBug className="mr-2" />{stats.open_issues}</p>
                     </div>
                 </section>
             </Suspense>
--- src/components/github/StatsList.tsx
+++ src/components/github/StatsList.tsx
@@ -3,7 +3,7 @@ import React, { useState, Suspense } from 'react';
 import { FaSort, FaArrowUp, FaArrowDown, FaSortNumericUp, FaSortNumericDown, FaSortNumericDownAlt, FaSortNumericUpAlt } from 'react-icons/fa';
 import { GitHubStatsResult } from '@/lib/github/stats'; // Adjust the import path as necessary
 import StatsCard from '@/components/github/Stats';
-import { FaCalendar, FaArchive, FaBoxOpen, FaGlobe, FaLock, FaQuestionCircle, FaCheckCircle, FaTimesCircle, FaStar, FaCodeBranch, FaEye, FaChartBar } from 'react-icons/fa';
+import { FaBug, FaHeartbeat, FaExclamationCircle, FaCalendar, FaArchive, FaBoxOpen, FaGlobe, FaLock, FaQuestionCircle, FaCheckCircle, FaTimesCircle, FaStar, FaCodeBranch, FaEye, FaChartBar } from 'react-icons/fa';
 import { capitalizeFirstLetter } from '@/lib/strings';
 
 interface StatsListCardProps {
@@ -15,11 +15,29 @@ interface ColumnNames {
     stars: JSX.Element;
     forks: JSX.Element;
     watchers: JSX.Element;
+    open_issues: JSX.Element;
 }
-
+const formatColumnName = (key: string) => {
+    switch (key) {
+        case 'health_percentage':
+            return 'Health';
+        case 'open_issues':
+            return 'Open issues';
+        default:
+            return capitalizeFirstLetter(key);
+    }
+};
+const iconMapping: { [key: string]: JSX.Element } = {
+    health_percentage: <FaHeartbeat className="mr-2" />,
+    stars: <FaStar className="mr-2" />,
+    forks: <FaCodeBranch className="mr-2" />,
+    watchers: <FaEye className="mr-2" />,
+    open_issues: <FaBug className="mr-2" />
+};
 const StatsListCard: React.FC<StatsListCardProps> = ({ reposStatsExtended }) => {
     const [statsList, setReposStats] = useState<GitHubStatsResult[]>(reposStatsExtended); // Initialize with your data
     const [sortConfig, setSortConfig] = useState<{ key: string, direction: 'asc' | 'desc' } | null>(null);
+    const [filterArchived, setFilterArchived] = useState<boolean>(true); // Filter archived items by default
 
     const sortData = (key: string, direction: 'asc' | 'desc') => {
         const sortedData = [...statsList].sort((a: GitHubStatsResult, b: GitHubStatsResult) => {
@@ -46,27 +64,45 @@ const StatsListCard: React.FC<StatsListCardProps> = ({ reposStatsExtended }) => 
         }
         return '';
     };
+    const handleFilterChange = () => {
+        setFilterArchived(!filterArchived);
+    };
     const columnNames: ColumnNames = {
         health_percentage: <span className="inline-flex items-center">Health Percentage</span>,
         stars: <span className="inline-flex items-center"><FaStar className="mr-2" />Stars</span>,
         forks: <span className="inline-flex items-center"><FaCodeBranch className="mr-2" />Forks</span>,
-        watchers: <span className="inline-flex items-center"><FaEye className="mr-2" />Watchers</span>
+        watchers: <span className="inline-flex items-center"><FaEye className="mr-2" />Watchers</span>,
+        open_issues: <span className="inline-flex items-center">Issues</span>
     };
     return (
         <Suspense fallback={<p>Loading data...</p>}>
-            <div className="container mx-auto p-4 bg-white shadow-md rounded-lg">
-                <div className="grid grid-cols-4 gap-4 mb-4 bg-gray-200 p-2 rounded">
+            <div className="container mx-auto p-5 bg-white shadow-md rounded-lg">
+                <div className="grid grid-cols-5 gap-4 mb-4 bg-gray-200 p-2 rounded">
                     {Object.keys(columnNames).map((key: string) => (
                         <div key={key} className="flex flex-col items-start w-full">
                             <button onClick={() => sortData(key, sortConfig?.key === key && sortConfig.direction === 'asc' ? 'desc' : 'asc')}>
-                                <span className={`text-lg ${getColumnClass(key)}`}>{capitalizeFirstLetter(key)}</span>
+                                <span className={`text-lg ${getColumnClass(key)}`}>
+                                    {iconMapping[key]} {formatColumnName(key)}
+                                </span>
                             </button>
                         </div>
                     ))}
+                    <div className="flex items-center col-span-1">
+                        <input
+                            type="checkbox"
+                            id="filterArchived"
+                            checked={filterArchived}
+                            onChange={handleFilterChange}
+                            className="mr-2"
+                        />
+                        <label htmlFor="filterArchived" className="text-lg">Filter Archived</label>
+                    </div>
                 </div>
-                {statsList.map((stat: GitHubStatsResult) => (
-                    <StatsCard key={stat.repo_name} ownerAndRepo={stat.repo_name} stats={stat.stats} metrics={stat.metrics} />
-                ))}
+                {statsList
+                    .filter(stat => !filterArchived || !stat.stats.archived)
+                    .map((stat: GitHubStatsResult) => (
+                        <StatsCard key={stat.repo_name} ownerAndRepo={stat.repo_name} stats={stat.stats} metrics={stat.metrics} />
+                    ))}
             </div>
         </Suspense>
     )
